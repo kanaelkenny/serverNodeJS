ddc36b44e585b4a25a733336f7d11f7d399a994f
kanaelkenny <kanael.planejamento@gmail.com>
2022-11-29 11:43:07 -0400

    Apenas para fins didatico

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var axios = require('axios');
+var cors = require('cors');
+
+app.use(express.json())
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    app.use(cors());
+    next()
+})
+
+app.get('/', async(req, res)=>{
+    try {
+        const { data } = await axios("https://docs.google.com/spreadsheets/d/e/2PACX-1vSZ-BpMDgK3ABlAznfqulUy_Yrf9ZyNP7ckcePwlRobhPS9G10ofxe4GfWrmMiGU2tLpswLBTMexQz7/pub?output=csv")
+        console.log(data)
+
+        return res.json(data)
+    } catch (error) {
+        console.error(error)   
+    }
+
+})
+
+app.get('/consulta', async(req, res)=>{
+    try {
+        const { data } = await axios("https://www.kooapp.com/apiV1/users/handle/kanael")
+        console.log(data)
+
+        return res.json(data)
+    } catch (error) {
+        console.error(error)   
+    }
+
+})
+
+app.listen(5600);
